7203f88f74464ec59c52ad2cc0ce823ea4f2aa9c
Anil Roy <anilroy.cce@gmail.com>
2026-06-21 23:45:48 +0530

    Upated file

--- 1.1 Using Node/index.js
+++ 1.1 Using Node/index.js
@@ -1,6 +1,6 @@
 const fs=require("fs");
 
-fs.writeFile("message.txt", "Hellow from Node JS, This file is created by using node.js in local computer by javaScript", (err)=>{
+fs.writeFile("message.txt", "Hello from Node JS, This file is created by using node.js in local computer by javaScript", (err)=>{
 
     if (err) throw err;
     console.log("File has been saved");
