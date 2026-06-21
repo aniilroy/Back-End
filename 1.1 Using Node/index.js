c0e81dff625610bd0c34181128b18b02e3b35cb4
Anil Roy <anilroy.cce@gmail.com>
2026-06-21 17:47:47 +0530

    Add index.js for node

--- 1.1 Using Node/index.js
+++ 1.1 Using Node/index.js
@@ -1,1 +1,8 @@
-console.log("Hello From Node");
+const fs=require("fs");
+
+fs.writeFile("message.txt", "Hellow from Node JS, This file is created by using node.js in local computer by javaScript", (err)=>{
+
+    if (err) throw err;
+    console.log("File has been saved");
+
+});
